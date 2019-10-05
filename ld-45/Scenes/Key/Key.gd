016fa4cf50554529114069522bc2af25d7cbc4f1
Unknown <hecobroe@gmx.de>
2019-10-05 19:36:37 +0200

    Added Basic Music and Collectable Key

---
+++ ld-45/Scenes/Key/Key.gd
@@ -0,0 +1,35 @@
+extends Sprite
+
+var Collected
+export (String) var playerName = "Player"
+
+func _ready():
+	set_visible(false)
+	Collected = false;
+
+#check for collision with Node called "Player"
+func _physics_process(delta):
+	if Collected == false:
+		var bodies = $Area2D.get_overlapping_bodies()
+		for b in bodies:
+			if b.get_name() == playerName:
+				collectBehaviour()
+
+func collectBehaviour():
+	get_parent().set_visible(false) 
+	Collected = true
+	collectSound()
+	get_parent().remove_child(self)
+	queue_free()
+
+#Returns the current state of the Gem
+func is_collected():
+	return Collected
+	
+func collectSound():
+	#var random = String(randi()%6+1)
+	#var path = "../GemSounds/GemSound" + random
+	#get_node(path).set_volume_db(-12.0) 
+	#print("Play sound: ", random)
+	#get_node(path).play(0.000001)
+	pass

3ea8d93909c0a1c9370bf0506fbd4264534589dc
leogeier <leonard.geier@hotmail.com>
2019-10-07 01:08:11 +0200

    Add interface to GUI for keybind updating

--- ld-45/Scenes/GUI/GUI.gd
+++ ld-45/Scenes/GUI/GUI.gd
@@ -4,6 +4,11 @@ onready var label_left = $Container/Keybinds/Left
 onready var label_right = $Container/Keybinds/Right
 onready var label_jump = $Container/Keybinds/Jump
 
-func _ready():
-	label_left.key = "a"
-	label_jump.key = "h"
+onready var labels = {
+	"left": $Container/Keybinds/Left,
+	"right": $Container/Keybinds/Right,
+	"jump": $Container/Keybinds/Jump
+}
+
+func update_keybind(label, key: String) -> void:
+	labels[label].set_key(key)

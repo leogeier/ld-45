dbbaa08ea1388e66c111bcec6dd602d0da905cf3
leogeier <leonard.geier@hotmail.com>
2019-10-06 15:37:41 +0200

    Save arenas configs as json

--- ld-45/Scenes/Arena/Arena.gd
+++ ld-45/Scenes/Arena/Arena.gd
@@ -10,4 +10,17 @@ func set_save_arena_config(value):
 	if not save_arena_config:
 		return
 	
-	print("saving")
+	var arena_config = JSON.print(get_arena_config())
+	var file_name = "config-" + String(OS.get_unix_time()) + ".json"
+	var file_path = "res://Scenes/Arena/arenaConfigs/" + file_name
+	var file = File.new()
+	
+	print("Saving arena config as " + file_name + "...")
+	
+	file.open(file_path, File.WRITE)
+	file.store_line(arena_config)
+	file.close()
+
+
+func get_arena_config() -> Dictionary:
+	return {"key": "value"}

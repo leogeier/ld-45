7cccaa09b7052d82e9ae6f52a1bbeea027126379
leogeier <leonard.geier@hotmail.com>
2019-10-07 18:52:56 +0200

    Animate doors

--- ld-45/Scenes/Door/Door.gd
+++ ld-45/Scenes/Door/Door.gd
@@ -3,7 +3,7 @@ extends Node2D
 
 export(bool) var closed = true setget set_closed, get_closed
 
-onready var sprite = $Sprite
+onready var sprite = $AnimatedSprite
 onready var staticBody = $StaticBody2D
 onready var collisionShape = $StaticBody2D/CollisionShape2D
 
@@ -17,11 +17,11 @@ func set_closed(value: bool) -> void:
 		return
 	
 	if closed:
-		sprite.show()
+		sprite.play("close")
 		staticBody.set_collision_layer_bit(0, true)
 		staticBody.set_collision_mask_bit(0, true)
 	else:
-		sprite.hide()
+		sprite.play("close", true)
 		staticBody.set_collision_layer_bit(0, false)
 		staticBody.set_collision_mask_bit(0, false)
 
